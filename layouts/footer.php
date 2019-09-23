29ab9d4e1d801f504965572e015bb95e3d205796
HunG <tanhunghue@gmail.com>
2019-09-23 08:17:58 +0700

    init app

---
+++ layouts/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 9/23/19
+ * Time: 8:01 AM
+ */
+?>
+
+
+<!-- Bootstrap core JavaScript-->
+<script src="template/vendor/jquery/jquery.min.js"></script>
+<script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="template/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Page level plugin JavaScript-->
+<script src="template/vendor/chart.js/Chart.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="template/js/sb-admin.min.js"></script>
+
+<!-- Demo scripts for this page-->
+<script src="template/js/demo/datatables-demo.js"></script>
+<script src="template/js/demo/chart-area-demo.js"></script>
+<script src="template/vendor/datatables/jquery.dataTables.js"></script>
+<script src="template/vendor/datatables/dataTables.bootstrap4.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="template/js/sb-admin.min.js"></script>
+
+<!-- Demo scripts for this page-->
+<script src="template/js/demo/datatables-demo.js"></script>
+<script src="template/js/demo/chart-area-demo.js"></script>
+
